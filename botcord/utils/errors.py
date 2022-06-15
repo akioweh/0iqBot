e7b5e4cc277d8fa966b9c8758a31a651908a0900
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-06-15 22:20:25 +0400

    new "protect" context manager to stop try-except spam

---
+++ botcord/utils/errors.py
@@ -0,0 +1,31 @@
+import traceback
+
+import sys
+
+
+class protect:
+    """
+    Context Manager used to suppress errors and print traceback to stderr
+    without halting program.
+    Protects whatever that is within the context.
+
+    Example::
+
+        with protect():
+            // protected code
+
+    """
+    def __init__(self, compact=False):
+        self.compact = compact
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if any((exc_type, exc_val, exc_tb)):
+            if self.compact:
+                print(exc_val, file=sys.stderr)
+            else:
+                print(f'Exception ingored and resuming execution of the remaining program:', file=sys.stderr)
+                traceback.print_exc()
+        return True
