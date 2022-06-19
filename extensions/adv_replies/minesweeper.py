07d1380002a47e639bcfd5294bc4d6d178135c35
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-06-19 11:46:25 +0400

    slight optimization

--- extensions/adv_replies/minesweeper.py
+++ extensions/adv_replies/minesweeper.py
@@ -1,5 +1,5 @@
 from random import sample
-from typing import TYPE_CHECKING
+from typing import Final, TYPE_CHECKING
 
 from discord.ext.commands import Context, command
 
@@ -10,18 +10,17 @@ if TYPE_CHECKING:
 
 
 class MineSweeper(Cog):
-    E = {
+    EMOJI: Final = {
         -1: '💥',
-        0: '⬛',
-        1: '1️⃣',
-        2: '2️⃣',
-        3: '3️⃣',
-        4: '4️⃣',
-        5: '5️⃣',
-        6: '6️⃣',
-        7: '7️⃣',
-        8: '8️⃣',
-        9: '9️⃣',
+        0 : '⬛',
+        1 : '1️⃣',
+        2 : '2️⃣',
+        3 : '3️⃣',
+        4 : '4️⃣',
+        5 : '5️⃣',
+        6 : '6️⃣',
+        7 : '7️⃣',
+        8 : '8️⃣',
     }
 
     def __init__(self, bot):
@@ -30,58 +29,50 @@ class MineSweeper(Cog):
     @command(aliases=['ms'])
     async def minesweeper(self, ctx: Context, width: int, height: int, mines: int):
         """Generates minesweeper board using spoilers."""
+        # Checks to ensure valid input
         grids = width * height
+        chars = grids * 7 + height - 1  # each square can take up to 7 unicode characters
         if width <= 0 or height <= 0:
-            await ctx.reply('stop tryna set invalid board sizes lol')
+            await ctx.reply('Board size is invalid.')
             return
-        chars = grids * 7 + height - 1
         if chars > 2000:
-            await ctx.reply('board is too big to fit within discord\'s 2000 character limit')
+            await ctx.reply('Board is too big to fit within discord\'s 2000 character limit.')
             return
         if mines > grids:
-            await ctx.reply('bros tryna fit more mines than there are squares')
+            await ctx.reply('There are more mines than squares on the board.')
             return
 
-        board_serial: list[int] = [0] * grids
-        mine_cords: list[int] = sample(list(range(grids)), mines)
-        for i in mine_cords:
-            board_serial[i] = -1
-            # update "bomb count" of neighboring squares
-            for j in MineSweeper.neighbors(width, height, i):
-                if board_serial[j] != -1:  # only increment bomb count if the square isn't a bomb lol
-                    board_serial[j] += 1
+        board: list[int] = [0] * grids
+        mine_pos_s: list[int] = sample(list(range(grids)), mines)
+        for mine_pos in mine_pos_s:
+            board[mine_pos] = -1  # -1 signifies bomb
+            # increment "bomb count" of neighboring squares
+            for n_pos in MineSweeper.neighbors(width, height, mine_pos):
+                if board[n_pos] != -1:  # only increment bomb count if the square isn't a bomb lol
+                    board[n_pos] += 1
 
         board_text = ''
-        for i, j in enumerate(board_serial):
-            if i % width == 0:
+        for pos, value in enumerate(board):
+            if pos % width == 0:  # Add newlines to make the board appear 2D in text
                 board_text += '\n'
-            board_text += f'||{MineSweeper.E[j]}||'
+            board_text += f'||{MineSweeper.EMOJI[value]}||'
         board_text.lstrip('\n')
 
-        print(board_text)
         await ctx.send(board_text)
 
     @staticmethod
-    def neighbors(x: int, y: int, pos: int):
-        up, down, left, right = False, False, False, False
-        mid = True
-        # up
-        if pos - x >= 0:
-            up = True
-        # down
-        if pos + x <= x * y - 1:
-            down = True
-        # left
-        if pos % x != 0:
-            left = True
-        # right
-        if pos % x != x - 1:
-            right = True
+    def neighbors(x: int, y: int, pos: int) -> list[int]:
+        # middle - up - down - left - right
+        mi: bool = True
+        up: bool = pos - x >= 0
+        dn: bool = pos + x <= x * y - 1
+        lt: bool = pos % x != 0
+        rt: bool = pos % x != x - 1
 
-        pos_bool = [left * up, mid * up, right * up, left * mid, mid * mid, right * mid, left * down, mid * down, right * down]
-        pos_pos = [pos - 1 - x, pos - x, pos + 1 - x, pos - 1, pos, pos + 1, pos - 1 + x, pos + x, pos + 1 + x]
-        neighbor_pos: list[int] = list(p for p, b in zip(pos_pos, pos_bool) if b)
-
+        pos_valid: list[bool] = [lt * up, mi * up, rt * up, lt * mi, False, rt * mi, lt * dn, mi * dn, rt * dn]
+        pos_list: list[int] = [pos - x - 1, pos - x, pos - x + 1, pos - 1, pos, pos + 1, pos + x - 1, pos + x,
+                               pos + x + 1]
+        neighbor_pos: list[int] = list(pos for pos, valid in zip(pos_list, pos_valid) if valid)
         return neighbor_pos
 
 
