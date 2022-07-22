c0bdf38bdb8de09dc86c7a35717f37fb958f70d2
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-07-22 15:09:18 +0400

    fix submission checks to actually detect all illegal cases

--- extensions/simple_games/twenty_four.py
+++ extensions/simple_games/twenty_four.py
@@ -16,6 +16,7 @@ if TYPE_CHECKING:
     from botcord import BotClient
 
 
+# noinspection SpellCheckingInspection
 class TwentyFour(Cog):
     def __init__(self, bot: 'BotClient'):
         self.bot = bot
@@ -53,36 +54,31 @@ class TwentyFour(Cog):
     async def submit(self, ctx: Context, *, answer: str):
         """Submit your answer to a 24-game question (by replying)
         and get feedback on whether it is correct or not"""
-        if not ctx.message.reference:
+        if not ctx.message.reference:  # make sure the message has a reference
             await ctx.reply('Reply to the question you are submitting the answer for', delete_after=5)
             return
-        if not isinstance(q_msg := ctx.message.reference.resolved, Message):
+        if not isinstance(q_msg := ctx.message.reference.resolved, Message):  # make sure the reference isn't fucked up
             await ctx.reply('Can\'t resolve the refererred message', delete_after=5)
             return
 
-        try:
+        try:  # parse the question
             q = list(int(i) for i in q_msg.content.strip('`').split())
             if len(q) != 4 or any((not 0 <= i <= 9) for i in q):
                 raise ValueError
-        except ValueError:
+        except ValueError:  # make sure the question is valid
             await ctx.reply('That doesn\'t seem like a valid 24-game question', delete_after=5)
             return
 
-        try:
-            ns = answer.replace('(', '').replace(')', '').replace('+', '').replace('-', '').replace('*', '')\
-                .replace('/', '').replace(' ', '')
-            if len(ns) != 4:
-                raise ValueError
-            for i in q:
-                ns = ns.replace(str(i), '', 1)
-            if ns != '':
-                raise ValueError
-        except ValueError:
-            await ctx.reply('That doesn\'t seem like a valid answer', delete_after=5)
+        if q_msg.author.id != self.bot.user.id:  # make sure the question was asked by the bot
+            await ctx.reply('Can\'t submit an answer to a question that wasn\'t made by me', delete_after=5)
             return
 
+        answer = answer.strip('`').strip()  # allow code block formatting
+
         try:
-            val = self.parser.parse(answer)
+            if '_' in answer:  # Python allows _ in number literals, such as 4_3 (=43), but let's not allow that
+                raise SyntaxError
+            val = self.parser.parse(answer)  # safely evaluate the answer
         except SyntaxError:
             await ctx.reply('Invalid expression syntax.', delete_after=5)
         except TypeError:
@@ -90,11 +86,36 @@ class TwentyFour(Cog):
         except ArithmeticError:
             await ctx.reply('Only four basic operations are allowed: + - * / \n'
                             '(and no leading minus because that\'s negation, not subtraction)', delete_after=5)
-        else:
-            if math.isclose(val, 24):
-                await ctx.reply(f'Correct answer. Took you `{ctx.message.created_at - q_msg.created_at}`')
+        else:  # expression equals 24, but we have more checks to do:
+            try:  # verify that the answer is valid; uses all four numbers, and only once each
+                operators = '+-*/'
+                numbers = "".join(q_msg.content.strip("`").split())
+                allowed_chars = operators + numbers
+                if not all(i in allowed_chars for i in answer):
+                    raise ValueError
+                clean = answer.replace('(', '').replace(')', '').replace(' ', '')  # only keep numbers and operators
+                # to make sure the four numbers are used legally, they must each appear once,
+                # delimited from each other by an operator
+                # the format of clean at this point should be 'xoxoxox' where x is a number and o is an operator
+                if not len(clean) == 7:
+                    raise ValueError
+                if not all(clean[i] in operators for i in (1, 3, 5)):  # make sure the operators are in their positions
+                    raise ValueError
+                if not all(clean[i] in numbers for i in (0, 2, 4, 6)):  # make sure the numbers are in their positions
+                    raise ValueError
+                nums = clean[0] + clean[2] + clean[4] + clean[6]  # the digits only as a 4-long string
+                if not all(i in nums for i in nums):  # make sure each number is used once and only once
+                    raise ValueError
+
+            except ValueError:
+                await ctx.reply('That doesn\'t seem like a valid answer', delete_after=5)
+                return
+
             else:
-                await ctx.reply(f'`{val}` is definitely not `24`; try harder.')
+                if math.isclose(val, 24):  # not much of a skill issue
+                    await ctx.reply(f'Correct answer. Took you `{ctx.message.created_at - q_msg.created_at}`')
+                else:  # massive skill issue.
+                    await ctx.reply(f'`{val}` is definitely not `24`; try harder.')
 
     # Computationally Intensive, lazy no brain implementation
     @staticmethod
