daf955714404fa36dc4007edf4f179503044916d
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-08-01 16:51:10 +0400

    BotCord: add custom ExtensionDisabledGuild error

--- botcord/__init__.py
+++ botcord/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'BotCord'
 __author__ = 'KEN_2000'
 __version__ = '1.0.0'
 
-from . import configs, checks
+from . import checks, configs, errors
 from .botclient import BotClient
 from .functions import *
-from .utils import str_info, find
+from .utils import find, str_info
