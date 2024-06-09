ea19ae235ef0f2c1b0ce82c4cf560364466e5f5c
akioweh <79527273+akioweh@users.noreply.github.com>
2024-06-09 22:50:06 +0400

    purger v1

---
+++ extensions/mod_utils/purger.py
@@ -0,0 +1,43 @@
+from asyncio import gather
+from typing import TYPE_CHECKING
+
+from discord import User, TextChannel
+from discord.ext.commands import command, Context
+
+from botcord.ext.commands import Cog
+
+if TYPE_CHECKING:
+    from botcord import BotClient
+
+
+class Purger(Cog):
+    def __init__(self, bot: 'BotClient'):
+        self.bot: 'BotClient' = bot
+
+    @command(aliases=['clear', 'cancel'])
+    async def obliterate(self, ctx: Context, user: User):
+        """Obliterate someone by deleting ALL their messages, ever."""
+        await ctx.send(f'Are you sure you want to obliterate {user.mention}? (yes/no)', delete_after=5)
+        reply = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=10)
+        if reply.content.lower() == 'yes':
+            await ctx.send(f'Obliterating {user.mention}...', delete_after=5)
+            await self._obliterate(ctx, user)
+        else:
+            await ctx.send('Obliteration cancelled.', delete_after=5)
+
+    @staticmethod
+    async def _obliterate(ctx: Context, user: User):
+        channels: list[TextChannel] = ctx.guild.text_channels
+        tasks = []
+        for channel in channels:
+            tasks.append(channel.purge(check=lambda m: m.author == user))
+        await gather(*tasks, return_exceptions=True)
+        deleted = 0
+        for task in tasks:
+            if isinstance(task, Exception):
+                await ctx.send('An error occurred while purging messages.')
+                raise task
+            else:
+                deleted += task
+
+        await ctx.send(f'Obliration complete. ({deleted} messages vaporized)', delete_after=5)
