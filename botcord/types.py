c4081cc83f44820b7cbc1d451f9689094276edf5
akioweh <79527273+akioweh@users.noreply.github.com>
2025-04-03 19:38:13 +0100

    update messagehook to work with new channel types (including Threads) and clean up

--- botcord/types.py
+++ botcord/types.py
@@ -1,10 +1,15 @@
-"""Typing Definitions for BotCord."""
+"""
+Typing Definitions for BotCord.
+"""
 
 from os import PathLike
 from typing import Protocol, Union, runtime_checkable
 
 __all__ = ['BasicTypes', 'ValueTypes', 'ConfigDict',
-           'StrOrPath', 'FileDescriptor', 'SupportsWrite']
+           'StrOrPath', 'FileDescriptor', 'SupportsWrite',
+           'WebhookMessagableChannel', 'WebhookPossessingChannel']
+
+from discord import ForumChannel, StageChannel, TextChannel, Thread, VoiceChannel
 
 type BasicTypes = str | int | float | bool | None
 type ValueTypes = BasicTypes | list[ValueTypes] | tuple[ValueTypes]
@@ -18,3 +23,9 @@ type FileDescriptor = StrOrPath | int
 class SupportsWrite[T](Protocol):  # copied-ish from typeshed
     def write(self, __s: T) -> object:
         ...
+
+
+# not using type statement because we want Union instead of TypeAlias, the former of which can be used with isinstance
+WebhookPossessingChannel = TextChannel | VoiceChannel | StageChannel | ForumChannel
+# although Thread and ForumChannel are special
+WebhookMessagableChannel = WebhookPossessingChannel | Thread
--- extensions/messagehook/messagehook.py
+++ extensions/messagehook/messagehook.py
@@ -3,9 +3,13 @@ from collections.abc import Callable
 from contextlib import suppress
 from typing import Sequence, TYPE_CHECKING
 
-from discord import AllowedMentions, Forbidden, HTTPException, Member, NotFound, TextChannel, User, Webhook
-from discord.ext.commands import Cog, Context, command
+from discord import AllowedMentions, Forbidden, ForumChannel, Guild, HTTPException, Member, NotFound, StageChannel, \
+    TextChannel, Thread, User, VoiceChannel, Webhook
+from discord.ext.commands import command
 from discord.utils import MISSING
+
+from botcord.ext.commands import Cog, Context
+from botcord.types import WebhookMessagableChannel, WebhookPossessingChannel
 
 if TYPE_CHECKING:
     from botcord import BotClient
@@ -18,47 +22,47 @@ class MessageHook(Cog):
         self.mentions = AllowedMentions(everyone=False, users=True, roles=False)
 
     @command(name='send', aliases=['say'])
-    async def _send_cmd(self, ctx: Context, *, text: str = None, delete: bool = True):
-        if text is None and ctx.message.attachments is None:
+    async def _send_cmd(self, ctx: Context, *, text: str | None = None, delete: bool = True):
+        if not text and not ctx.message.attachments:
             return
-        if len(text) > 2000:
+        if text is not None and len(text) > 2000:
             await ctx.reply('Messages must be 2000 or fewer characters in length. (nitro abuse smh)', delete_after=5)
             return
 
         attachments = await asyncio.gather(*(item.to_file() for item in ctx.message.attachments))
 
-        if delete:
-            delete = asyncio.create_task(ctx.message.delete())
+        delete_task = asyncio.create_task(ctx.message.delete()) if delete else None
+
         with suppress(Forbidden):
             await ctx.send(content=text,
                            files=attachments,
-                           reference=ctx.message.reference,
+                           reference=ctx.message.reference,  # type: ignore # what
                            allowed_mentions=self.mentions)
-        if delete:
+        if delete_task is not None:
             with suppress(Forbidden, NotFound):
-                await delete
+                await delete_task
 
     @command(name='resend', aliases=['repost'])
-    async def _resend_cmd(self, ctx: Context, *, text: str = None):
-        # new discord.py typing annotations for @command are too complicated for PyCharm?!!!
-        # noinspection PyTypeChecker
+    async def _resend_cmd(self, ctx: Context, *, text: str | None = None):
         await self._sendas_cmd(ctx, ctx.author, text=text)
 
     @command(name='sendas', aliases=['sayas'])
-    async def _sendas_cmd(self, ctx: Context, user: User | Member, *, text: str = None, delete: bool = True):
-        if text is None and ctx.message.attachments is None:
+    async def _sendas_cmd(self, ctx: Context, user: User | Member, *, text: str | None = None, delete: bool = True):
+        if not isinstance(ctx.channel, TextChannel | VoiceChannel | StageChannel | ForumChannel | Thread):
+            await ctx.reply('This channel does not seem to support webhooks. ')
             return
-        if len(text) > 2000:
+        if not text and not ctx.message.attachments:
+            return
+        if text is not None and len(text) > 2000:
             await ctx.reply('Messages must be 2000 or fewer characters in length. (nitro abuse smh)', delete_after=5)
             return
 
         attachments = await asyncio.gather(*(item.to_file() for item in ctx.message.attachments))
 
-        if delete:
-            delete = asyncio.create_task(ctx.message.delete())
+        delete_task = asyncio.create_task(ctx.message.delete()) if delete else None
 
         try:
-            await MessageHook.send(ctx.channel, text, user.name, user.avatar.url, attachments, self.mentions)
+            await MessageHook.send(ctx.channel, text, user.name, user.display_avatar.url, attachments, self.mentions)
         except Forbidden:
             with suppress(Forbidden):
                 await ctx.reply('Missing Permissions', delete_after=5)
@@ -66,54 +70,59 @@ class MessageHook(Cog):
             if error.code == 30007:
                 await ctx.reply('All existing webhooks are unusable (please delete). '
                                 'Failed to create new one: Maximum number of webhooks in this channel reached (10).')
-        if delete:
+        if delete_task is not None:
             with suppress(Forbidden, NotFound):
-                await delete
+                await delete_task
 
     @command(name='cleanhooks')
     async def _cleanhooks_cmd(self, ctx: Context):
-        if not ctx.guild:
+        if ctx.guild is None:
             return
         try:
             check = lambda hook: hook.name == 'MessageHook' or hook.user == ctx.bot.user
-            deleted = await MessageHook.clean_hooks(ctx.channel, check)
+            deleted = await MessageHook.clean_hooks(ctx.guild, check)
             await ctx.reply(f'Deleted {deleted} webhooks.')
         except Forbidden:
             with suppress(Forbidden):
                 await ctx.reply('Missing Permissions', delete_after=5)
 
     @staticmethod
-    async def clean_hooks(chl: TextChannel, check: Callable[[Webhook], bool]) -> int:
-        hooks: list[Webhook] | None = await chl.guild.webhooks()
-        delete_ables = [hook for hook in hooks if check(hook)]
-        if not delete_ables:
+    async def clean_hooks(guild: Guild, check: Callable[[Webhook], bool]) -> int:
+        """Deletes all webhooks in a guild that match the given check function."""
+        hooks = await guild.webhooks()
+        hooks_to_delete = list(filter(check, hooks))
+        if not hooks_to_delete:
             return 0
 
-        await asyncio.gather(*(hook.delete() for hook in delete_ables))
-        return len(delete_ables)
+        await asyncio.gather(*(hook.delete() for hook in hooks_to_delete))
+        return len(hooks_to_delete)
 
     @staticmethod
-    async def send(chl: TextChannel,
-                   content: str,
+    async def send(chl: WebhookMessagableChannel,
+                   content: str | None,
                    username: str,
                    avatar_url: str,
-                   attachments: Sequence = None,
+                   attachments: Sequence | None = None,
                    allowed_mentions: AllowedMentions = AllowedMentions.none()):
         """Sends a message to a channel with a webhook."""
-        hooks: list[Webhook] | None = await chl.webhooks()
-        valid_hook: Webhook | None = None
+        parent_channel: WebhookPossessingChannel | None = chl.parent if isinstance(chl, Thread) else chl
+        if parent_channel is None:
+            raise ValueError('Cannot find valid channel from chl.')
+
+        hooks = await parent_channel.webhooks()
+        valid_hook = None
         for hook in hooks:
             if hook.token:
                 valid_hook = hook
                 break
-
         if not valid_hook:
-            valid_hook = await chl.create_webhook(name='MessageHook')
+            valid_hook = await parent_channel.create_webhook(name='MessageHook', reason='MessageHook')
 
-        await valid_hook.send(content=content,
+        await valid_hook.send(content=content or MISSING,
                               username=username,
                               avatar_url=avatar_url,
                               files=attachments or MISSING,
+                              thread=chl if isinstance(chl, Thread) else MISSING,
                               allowed_mentions=allowed_mentions)
 
 
